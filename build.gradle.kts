f41f9902a84a0236351b326ce76a5bf7fff16c59
monun <monun1010@gmail.com>
2021-05-29 19:52:09 +0900

    :hammer: ProtocolLib 주석으로 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     compileOnly(kotlin("stdlib"))
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    //    compileOnly("com.github.monun:invfx:2.0.0")
+//    compileOnly("com.github.monun:invfx:2.0.0")
 
     implementation("com.github.monun:tap:3.6.0")
     implementation("com.github.monun:kommand:1.0.0")
@@ -30,6 +30,14 @@ dependencies {
     testImplementation("org.mockito:mockito-core:3.6.28")
     testImplementation("org.spigotmc:spigot:1.16.5-R0.1-SNAPSHOT")
 }
+
+/*//ProtocolLib
+repositories {
+    maven("https://repo.dmulloy2.net/repository/public/")
+}
+dependencies {
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0")
+}*/
 
 tasks {
     withType<JavaCompile> {
