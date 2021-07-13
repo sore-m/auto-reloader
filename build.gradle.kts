a367e1ec90a85fae4b8233a5d09fd870ad323f74
Monun <monun1010@gmail.com>
2021-07-14 08:54:58 +0900

    :arrow_up: 1.17.1 업데이트 끝

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.5.20"
-    kotlin("plugin.serialization") version "1.5.20"
+    kotlin("jvm") version "1.5.21"
+    kotlin("plugin.serialization") version "1.5.21"
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
@@ -20,8 +20,8 @@ dependencies {
 
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-    implementation("io.github.monun:kommand-api:2.0.1")
-    implementation("io.github.monun:tap-api:4.0.0")
+    implementation("io.github.monun:tap-api:4.1.1")
+    implementation("io.github.monun:kommand-api:2.1.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
